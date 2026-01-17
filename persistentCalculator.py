41d5a92bd9f6e4d34fed00ae37ee484e5f495b7a
James Xander A. Yugo <yugo.jamesxander8871@gmail.com>
2026-01-17 09:21:42 +0000

    Added loop, addition function

--- persistentCalculator.py
+++ persistentCalculator.py
@@ -1,1 +1,50 @@
 
+continue_calculation = True
+result = 0
+
+def addition(number, result):
+    return result + number
+    
+# Put subtraction function here
+
+# Put multiplication 
+
+# Put division function here
+
+print("Enter +, -, *, or / to start operating.")
+print("Enter 'q' to quit.")
+print("Enter 'c' to clear result")
+
+while continue_calculation:
+
+    print (f"\nCurrent result: {result}")
+    op = input("Enter operation: ")
+
+    if op == 'q':
+        continue_calculation = False
+
+    elif op == 'c':
+        result = 0
+        print("Result cleared")
+
+    elif op in {"+", "-", "*", "/"}:
+
+        try:
+            num = float(input("Enter Number: "))
+
+            if op == '+':
+                result = addition(num, result)
+            
+            # Put subtraction elif here
+
+            # Put multiplication elif here
+
+            # Put division elif here
+
+        except ValueError:
+            print("Invalid Number")
+
+    else:
+        print("Invalid Operation")
+
+print("Calculator Closed")
