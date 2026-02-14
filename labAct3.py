da1a9a12dc1f460f80c3a02ccdc3fa025ceef41d
RinJ07 <127010088+RinJ07@users.noreply.github.com>
2026-02-14 18:37:06 +0800

    Implement compute_payroll function for salary calculations
    Added compute_payroll function to calculate deductions and net salary.

--- labAct3.py
+++ labAct3.py
@@ -17,3 +17,16 @@ def compute_pagibig(salary):
 def compute_tax(salary):
     print(f"Tax computed by {threading.current_thread().name}")
     return salary * TAX_RATE
+    
+def compute_payroll(employee):
+    name, salary = employee
+
+    sss = salary * SSS_RATE
+    philhealth = salary * PHILHEALTH_RATE
+    pagibig = salary * PAGIBIG_RATE
+    tax = salary * TAX_RATE
+
+    total_deduction = sss + philhealth + pagibig + tax
+    net_salary = salary - total_deduction
+
+    return name, salary, total_deduction, net_salary
